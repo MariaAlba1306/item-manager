911879dd1df0d8a31108dc59864ea3b7275d4c62
MariaAlba1306 <maria.alba@wallapop.com>
2023-09-08 21:34:12 +0200

    api layer added

---
+++ src/app/api/api-service.ts
@@ -0,0 +1,16 @@
+export interface Item {
+  title: string;
+  price: number;
+  description: string;
+  image: string;
+  email: string;
+}
+export const getItemsList = async (): Promise<Item[]> => {
+  const API_URL =
+    "https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/items.json";
+  const result = await fetch(API_URL);
+  const json = await result.json();
+  const itemList = json.results;
+
+  return itemList;
+};
